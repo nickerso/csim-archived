bcb0d0977f92189d429b9559176c301a2814c552
David Nickerson <nickerso@users.sourceforge.net>
2011-12-21 17:44:18 +1300

    adding a couple of new source files ready to start working on extracting the simulation parameters from the source XML document

---
+++ src/xpath.c
@@ -0,0 +1,12 @@
+/*
+ * xpath.c
+ *
+ *  Created on: Dec 21, 2011
+ *      Author: dnic019
+ */
+
+#include "xpath.h"
+
+/*
+ * Need to be able to load in the XML source and create a simulation
+ */
---
+++ src/xpath.h
@@ -0,0 +1,13 @@
+/*
+ * xpath.h
+ *
+ *  Created on: Dec 21, 2011
+ *      Author: dnic019
+ */
+
+#ifndef XPATH_H_
+#define XPATH_H_
+
+
+
+#endif /* XPATH_H_ */
